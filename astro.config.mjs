284c2d2737cc21ae71546af365f1fa7e3f5c82ca
David Thyresson <dthyresson@gmail.com>
2025-02-04 11:44:16 -0500

    see if site uses netlify envs

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   prefetch: true,
   output: 'static',
   adapter: netlify(),
-  site: 'https://example.com',
+  // site: 'https://example.com',
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
