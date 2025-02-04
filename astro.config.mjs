f314d90b485d1ef9a089c20f92db854b63274c7c
David Thyresson <dthyresson@gmail.com>
2025-02-04 11:51:18 -0500

    netlify deploy urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   prefetch: true,
   output: 'static',
   adapter: netlify(),
-  // site: 'https://example.com',
+  site: process.env.DEPLOY_URL || process.env.URL || 'http://localhost:4321',
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
